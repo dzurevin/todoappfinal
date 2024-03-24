60f1839cfdf6e8d919981b9d6e7feabbac4961e6
dzurevin <dzurevin@confederationcollege.ca>
2024-03-24 16:17:31 -0400

    sun mar 24th

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,14 @@
-
-
-
-
 <svelte:head>
-    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+     <link
+     rel="stylesheet"
+     href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.amber.min.css"
+   />
 </svelte:head>
 
 <script>
      import '../style.css'
 
-    let toDoList = []; // array of ToDos
+    let toDoList = []; // array of To Dos
     let textInput = "";
 
     function addToDo() {
@@ -18,47 +17,49 @@
     }
 
     function setEditing(i, isEditing) {
-        toDoList[i].editing = isEditing; // true / false
+        toDoList[i].editing = isEditing; 
     }
 
     function deleteTodo(i) {
         toDoList.splice(i, 1);
-        toDoList = toDoList; // https://svelte.dev/tutorial/updating-arrays-and-objects
+        toDoList = toDoList; 
     }
 </script>
-
-<div style="margin: 0 auto; padding: 20px; width: 700px;">
+<div class="main"> 
+<div style="margin: 0 auto; padding-top: 5em; width: 700px;">
     <h2 style="text-align: center;">To Do List</h2>
     <p>Enter your upcoming commitments here</p>
     <div style="display: flex; margin: 0;">
         <input style="margin: 0; width: 35em;" type="text" bind:value={textInput}>
-        <button style="margin-left: 1em; width: 200px;" on:click={addToDo}>Add</button>
+        <button style="margin-left: 1em; width: 11em;" on:click={addToDo}>Add</button>
     </div>
 </div>
 
 {#each toDoList as toDo, i}
-    <div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto; margin-bottom: 1em;">
+    <div style="display: flex; align-items: baseline; width: 700px; margin: 0 auto; margin-top: 1em;">
         {#if toDo.editing}
             <input type="text" bind:value={toDo.content}>
         {:else}
             <input type="checkbox" bind:checked={toDo.checked}>
-            <h4 style="flex-grow: 1">{toDo.content}</h4>
+            <h4 style="flex-grow: 1;">{toDo.content}</h4>
         {/if}
         <div style="display: flex">
             {#if toDo.editing}
-                <button on:click={() => setEditing(i, false)}>Save</button>
+                <button style="margin-left: 0.7em;" on:click={() => setEditing(i, false)}>Save</button>
             {:else}
-                <button on:click={() => setEditing(i, true)}>Edit</button>
+                <button style="margin-left: 0.7em;" on:click={() => setEditing(i, true)}>Edit</button>
             {/if}
             <button style="margin-left: 0.7em;" on:click={() => deleteTodo(i)}>Delete</button>
         </div>
     </div>
 {/each}
 
+<div id="footer-container">
+     <footer id="footer"></footer>
+</div>
 
 
-
-
+</div>
 
 
 
